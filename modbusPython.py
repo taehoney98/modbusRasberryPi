ddc5ad0882cce1f95e3faf4111eb481cbf121725
taehoney98 <jkee754@gmail.com>
2022-05-31 17:17:31 +0900

    coil/register 자동 측정

--- modbusPython.py
+++ modbusPython.py
@@ -1,6 +1,7 @@
 from EasyModbusPy.easymodbus.modbusClient import *
 import time
 import schedule
+import random
 
 modbus_client =ModbusClient('192.168.0.39',502)
 modbus_client.parity=Parity.even
@@ -20,16 +21,26 @@ def writeCoil(index):
         modbus_client.write_single_coil(index, False)
     index=index+1    
 
-def readCoil():
-    print(modbus_client.read_coils(0,10))
+def writeRegister(index):
+    holding_register = modbus_client.read_holdingregisters(0,10)
+    if index >= 10:
+        index=index-10
+    if holding_register[index] != None:
+        modbus_client.write_single_register(index, random.randint(0,32767))
 
+def read():
+    print(modbus_client.read_coils(0,10))
+    print(modbus_client.read_holdingregisters(0,10))
+    
 
 for i in range(10):
     schedule.every(1).seconds.do(writeCoil,i)
+    schedule.every(1).seconds.do(writeRegister,i)
 
-schedule.every(10).seconds.do(readCoil)
+schedule.every(10).seconds.do(read)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+    
 modbus_client.close()
