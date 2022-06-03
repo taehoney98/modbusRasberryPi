ef04fbdf1fc396dce021a71b05b85da7e93846c0
taehoney98 <81626703+taehoney98@users.noreply.github.com>
2022-06-03 17:45:59 +0900

    Delete easymodbus directory

--- easymodbus/__init__.py
+++
@@ -1,3 +0,0 @@
-#Only required for Python 2:
-#We Make an empty file called __init__.py in the same directory as the files.
-#That will signify to Python that it's "ok to import from this directory".
