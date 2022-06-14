31a095bec48677032fb4fb9f4efbfd9d0a72a78d
taehoney98 <jkee754@gmail.com>
2022-06-14 10:35:28 +0900

    ALLOWED_HOST 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '86dibweiqm=(j(ytnd$t8z)d=ey(y_sixc9@0o&qr1fgf)gra)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.38.3.27']
 
 
 # Application definition
